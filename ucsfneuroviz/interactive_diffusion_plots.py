0ebbe526439008ea9d54b5483cbd0050959a51df
Margo Kersey <margokersey@gmail.com>
2023-09-29 15:02:28 -0700

    add html

--- ucsfneuroviz/interactive_diffusion_plots.py
+++ ucsfneuroviz/interactive_diffusion_plots.py
@@ -1,13 +1,22 @@
-# %%
-import os
-import nibabel as nib
-import plotly
-import numpy as np
-from dipy.io.streamline import load_tractogram
-from dipy.io.stateful_tractogram import Space
-from AFQ.utils.streamlines import SegmentedSFT
-from AFQ.viz.plotly_backend import visualize_bundles, visualize_volume
+import ipywidgets as widgets
+from IPython.display import display, HTML
 
 
-working_dir = '/Volumes/language/dyslexia/browser_development/qsiprep'
-tract_base_path = os.path.join(working_dir, 'sub-34429_recon/ses-20230317/dwi/sub-34429_ses-20230317_space-T1w_desc-preproc/clean_bundles/')
+def generate_legend():
+    legend_content = """
+    <div>
+        <div style="display: flex; flex-direction: column;">
+            <div><strong>Legend</strong></div>
+            <div><span style="background-color: #FF5733; padding: 2px 10px;"></span> Abbreviation 1</div>
+            <div><span style="background-color: #33FF57; padding: 2px 10px;"></span> Abbreviation 2</div>
+            <!-- Add more entries as needed -->
+        </div>
+    </div>
+    """
+    
+    legend_widget = HTML(legend_content)
+    
+    return legend_widget
+
+# Create a legend widget
+legend = generate_legend()
